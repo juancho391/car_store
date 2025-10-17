678a3cda45c104f96089d5f41205fdfc2b938e8d
juancho391 <juanchischis2010@hotmail.com>
2025-10-17 12:42:15 -0500

    forms for signup, login and car

---
+++ src/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class SignupForm(FlaskForm):
+    name = StringField("Nombre", validators=[DataRequired(), Length(max=64)])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Registrar")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
+    remember_me = BooleanField("Recuérdame")
+    submit = SubmitField("Login")
+
+
+class CarForm(FlaskForm):
+    make = StringField("Marca", validators=[DataRequired(), Length(max=80)])
+    model = StringField("Modelo", validators=[DataRequired(), Length(max=80)])
+    year = StringField("Año", validators=[DataRequired()])
+    price = StringField("Precio", validators=[DataRequired()])
+    submit = SubmitField("Guardar")
